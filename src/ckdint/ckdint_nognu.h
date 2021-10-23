7848ab18d740ff11a116df4587a3e1e6672ecf16
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-10-24 00:43:08 +0200

    ckdint_nognu: fix side effects and problems, enhance, refactor

--- src/ckdint/ckdint_nognu.h
+++ src/ckdint/ckdint_nognu.h
@@ -3,7 +3,11 @@
  * @file ckdint_nognu.h
  * @author Kamil Cukrowski <kamilcukrowski@gmail.com>
  * @date 2021-06-19
+ * @copyright 2021 Kamil Cukrowski
  * SPDX-License-Identifier: MIT + Beerware
+ *
+ * While this file tries not to use any GNU extensions, it
+ * strongly assumes that all types are twos-complement.
  */
 // Header [[[
 #ifndef CKDINT_H_
@@ -24,12 +28,23 @@
 #ifndef UINT128_MAX
 #define UINT128_MAX  ((2 * (unsigned __int128) INT128_MAX) + 1)
 #endif
-#endif // __SIZEOF_INT128__
+#endif  // __SIZEOF_INT128__
+
+{% call() L.foreach_TYPE() %}
+typedef struct {
+	/// Represents the stored value.
+	_ckd_$TYPE v;
+	/// Overflow?
+	bool o;
+	/// Was the original type signed?
+	bool s;
+} _ckd_arg_$TYPE;
+{% endcall %}
 
 // ]]]
 {% for N, T, DETECT, MAX, MIN in L.TYPES %}
 	{% if MIN != "0" %}{# Only signed types #}
-		{% set HALFIDX = loop.index / 2 | int * 2 %}
+		{% set HALFIDX = ((((loop.index / 2) | int) * 2) | int) %}
 		{# Signed key #}
 		{% set S = N %}
 		{# Unsigned key #}
@@ -53,7 +68,7 @@
 // ]]]
 // Addition and substraction operations [[[
 
-// from https://github.com/gcc-mirror/gcc/blob/16e2427f50c208dfe07d07f18009969502c25dc8/gcc/internal-fn.c#L713
+{# from https://github.com/gcc-mirror/gcc/blob/16e2427f50c208dfe07d07f18009969502c25dc8/gcc/internal-fn.c#L713 #}
 _ckd_fchpnt bool _ckd_add_sss_{{N}}(U s1, U s2, U *res) {
 	*res = s1 + s2;
 	return (S)s2 < 0 ? (S)*res > (S)s1 : (S)*res < (S)s1;
@@ -61,10 +76,9 @@ _ckd_fchpnt bool _ckd_add_sss_{{N}}(U s1, U s2, U *res) {
 
 _ckd_fchpnt bool _ckd_sub_sss_{{N}}(U s1, U s2, U *res) {
 	*res = s1 - s2;
-	// taken from
-	// https://github.com/gcc-mirror/gcc/blob/16e2427f50c208dfe07d07f18009969502c25dc8/gcc/internal-fn.c#L1058
+	{# https://github.com/gcc-mirror/gcc/blob/16e2427f50c208dfe07d07f18009969502c25dc8/gcc/internal-fn.c#L1058 #}
 	return (s1 ^ s2) & ~(*res ^ s2) & _ckd_SGN;
-	// return s2 < 0 ? *res < s1 : *res > s2;
+	{# return s2 < 0 ? *res < s1 : *res > s2; #}
 }
 
 _ckd_fchpnt bool _ckd_add_uuu_{{N}}(U u1, U u2, U *res) {
@@ -141,38 +155,17 @@ _ckd_fchpnt bool _ckd_add_usu_{{N}}(U u1, U s1, U *res) {
 // ]]]
 // Multiplication [[[
 
-// https://wiki.sei.cmu.edu/confluence/display/c/INT32-C.+Ensure+that+operations+on+signed+integers+do+not+result+in+overflow
+{# https://wiki.sei.cmu.edu/confluence/display/c/INT32-C.+Ensure+that+operations+on+signed+integers+do+not+result+in+overflow #}
 _ckd_fconst bool _ckd_ovf_signed_{{N}}(S a, S b) {
-	if (a > 0) {
-		if (b > 0) {
-			if (a > {{MAX}} / b) { // a positive, b positive
-				return 1;
-			}
-		} else {
-			if (b < {{MIN}} / a) { // a positive, b not positive
-				return 1;
-			}
-		}
-	} else {
-		if (b > 0) {
-			if (a < {{MIN}} / b) { // a not positive, b positive
-				return 1;
-			}
-		} else {
-			if (a != 0 && b < {{MAX}} / a) { // a not positive, b not positive
-				return 1;
-			}
-		}
-	}
-	return 0;
+	return a > 0 ? b > 0 ? (a > {{MAX}} / b) : (b < {{MIN}} / a) : b > 0 ? (a < {{MIN}} / b) : (a != 0 && b < {{MAX}} / a);
 }
 
-// https://stackoverflow.com/a/1815391/9072753
+{# https://stackoverflow.com/a/1815391/9072753 #}
 _ckd_fconst bool _ckd_ovf_unsigned_{{N}}(U a, U b) {
 	return a != 0 && b > {{UMAX}} / a;
 }
 
-// https://github.com/gcc-mirror/gcc/blob/16e2427f50c208dfe07d07f18009969502c25dc8/gcc/internal-fn.c#L1286
+{# https://github.com/gcc-mirror/gcc/blob/16e2427f50c208dfe07d07f18009969502c25dc8/gcc/internal-fn.c#L1286 #}
 
 _ckd_fchpnt bool _ckd_mul_sss_{{N}}(U s1, U s2, U *res) {
 	*res = s1 * s2;
@@ -217,57 +210,55 @@ _ckd_fchpnt bool _ckd_mul_usu_{{N}}(U u1, U s2, U *res) {
 /* ------------------------------------------------------------------------- */
 
 {% call() L.foreach_OP() %}
-static bool (*const _ckd_$OP_{{N}}_choose_arr[])(U, U, U*) = {
-	&_ckd_$OP_sss_{{N}},
-	&_ckd_$OP_ssu_{{N}},
-	&_ckd_$OP_sus_{{N}},
-	&_ckd_$OP_suu_{{N}},
-	&_ckd_$OP_uss_{{N}},
-	&_ckd_$OP_usu_{{N}},
-	&_ckd_$OP_uus_{{N}},
-	&_ckd_$OP_uuu_{{N}},
-};
-
-_ckd_fchpnt bool _ckd_$OP_{{N}}_choose(unsigned idx, U a, U b, U *res) {
-	return _ckd_$OP_{{N}}_choose_arr[idx](a, b, res);
+_ckd_fchpnt bool _ckd_$OP_{{N}}_choose(bool asigned, bool bsigned, bool ressigned, U a, U b, U *res) {
+	static bool (*const _ckd_$OP_{{N}}_choose_arr[])(U, U, U*) = {
+		&_ckd_$OP_uuu_{{N}},
+		&_ckd_$OP_uus_{{N}},
+		&_ckd_$OP_usu_{{N}},
+		&_ckd_$OP_uss_{{N}},
+		&_ckd_$OP_suu_{{N}},
+		&_ckd_$OP_sus_{{N}},
+		&_ckd_$OP_ssu_{{N}},
+		&_ckd_$OP_sss_{{N}},
+	};
+	return _ckd_$OP_{{N}}_choose_arr[asigned << 2 | bsigned << 1 | ressigned](a, b, res);
 }
 
-_ckd_fconst _ckd_c{{S}} _ckd_$OP_2_{{S}}(unsigned idx, U a, U b, bool other) {
+_ckd_fconst _ckd_c{{S}} _ckd_$OP_2_{{S}}(_ckd_arg_{{S}} a, _ckd_arg_{{S}} b) {
 	_ckd_c{{S}} tmp;
-	ckd_overflow(tmp) = _ckd_$OP_{{N}}_choose(idx, a, b, (void*)&ckd_value(tmp)) || other;
+	ckd_overflow(tmp) = _ckd_$OP_{{N}}_choose(a.s, b.s, 1, a.v, b.v, (U*)&ckd_value(tmp)) || a.o || b.o;
 	return tmp;
 }
 
-_ckd_fconst _ckd_c{{U}} _ckd_$OP_2_{{U}}(unsigned idx, U a, U b, bool other) {
+_ckd_fconst _ckd_c{{U}} _ckd_$OP_2_{{U}}(_ckd_arg_{{U}} a, _ckd_arg_{{U}} b) {
 	_ckd_c{{U}} tmp;
-	ckd_overflow(tmp) = _ckd_$OP_{{N}}_choose(idx, a, b, (void*)&ckd_value(tmp)) || other;
+	ckd_overflow(tmp) = _ckd_$OP_{{N}}_choose(a.s, b.s, 0, a.v, b.v, &ckd_value(tmp)) || a.o || b.o;
 	return tmp;
 }
 
 	{% call(A) L.foreach_TYPE(arg=1) %}
-_ckd_fchpnt bool _ckd_$OP_3_{{S}}_to_$TYPE(_ckd_$TYPE *ret, unsigned idx, U a, U b, bool other) {
+_ckd_fchpnt bool _ckd_$OP_3_{{S}}_to_$TYPE(_ckd_$TYPE *ret, _ckd_arg_{{S}} a, _ckd_arg_{{S}} b) {
 	_ckd_{{U}} tmp;
-	const bool opovf = _ckd_$OP_{{N}}_choose(idx, a, b, &tmp);
-	*ret = tmp;
-	return opovf || other
-		{% if HALFIDX > A.HALFIDX %}
+	const bool opovf = _ckd_$OP_{{N}}_choose(a.s, b.s, {{A.SIGNED}}, a.v, b.v, &tmp)
+#if {{MIN}} < {{A.MIN}}
 			|| (S) {{A.MIN}} > (S) tmp
+#endif
+#if {{MAX}} > {{A.MAX}}
 			|| (S) tmp > (S) {{A.MAX}}
-		{% endif %}
-			;
+#endif
+			|| a.o || b.o;
+	*ret = (_ckd_$TYPE) tmp;
+	return opovf;
 }
-_ckd_fchpnt bool _ckd_$OP_3_{{U}}_to_$TYPE(_ckd_$TYPE *ret, unsigned idx, U a, U b, bool other) {
+_ckd_fchpnt bool _ckd_$OP_3_{{U}}_to_$TYPE(_ckd_$TYPE *ret, _ckd_arg_{{U}} a, _ckd_arg_{{U}} b) {
 	_ckd_{{U}} tmp;
-	const bool opovf = _ckd_$OP_{{N}}_choose(idx, a, b, &tmp);
-	*ret = tmp;
-	return opovf || other
-		{% if HALFIDX > A.HALFIDX %}
-		{% if A.MIN != "0" %}
-			|| (U) {{A.MIN}} > tmp
-		{% endif %}
-			|| tmp > (U) {{A.MAX}}
-		{% endif %}
-			;
+	const bool opovf = _ckd_$OP_{{N}}_choose(a.s, b.s, {{A.SIGNED}}, a.v, b.v, &tmp)
+#if {{MAX}} > {{A.MAX}}
+			|| (U) tmp > (U) {{A.MAX}}
+#endif
+			|| a.o || b.o;
+	*ret = (_ckd_$TYPE) tmp;
+	return opovf;
 }
 	{% endcall %}
 {% endcall %}
@@ -284,33 +275,63 @@ _ckd_fchpnt bool _ckd_$OP_3_{{U}}_to_$TYPE(_ckd_$TYPE *ret, unsigned idx, U a, U
 
 #define _ckd_issigned(T) \
 		_Generic(_ckd_value(T), \
-{% call(d) L.foreach_TYPE(inmacro=1, arg=1) %}
-		_ckd_$TYPE: {{ d.signed }}
+{% call(D) L.foreach_TYPE(inmacro=1, arg=1) %}
+		_ckd_$TYPE: {{ D.SIGNED }}
 {%- endcall %})
 
-#define _ckd_idx(a, b)  ( \
-		_ckd_issigned(a) << 2 | \
-		_ckd_issigned(b) << 1 | \
-		_ckd_issigned(_ckd_value(a) + _ckd_value(b)) \
-		)
+{% call() L.foreach_TYPE() %}
+	{% call(T2) L.foreach_TYPE(repl="$TYPE2", arg=1) %}
+_ckd_fconst _ckd_arg_$TYPE _ckd_c$TYPE2_to_arg_$TYPE(_ckd_c$TYPE2 _ckd_v) {
+	const _ckd_arg_$TYPE _ckd_ret = { ckd_value(_ckd_v), ckd_overflow(_ckd_v), {{T2.SIGNED}} }; return _ckd_ret;
+}
+	{% endcall %}
+{% endcall %}
+
+/**
+ * As of now, this is the part that makes nested invokations not possible to compile.
+ * This is the slowest part because of the double _Generic(...: _Generic(....) );
+ * I have no really good idea how to remove it. The problem is that `FROM` is macro expanded
+ * by preprocessor for each type, which makes the preprocessor output unbearable big if `FROM`
+ * is also a call to ckd_* function.
+ * The arguments to the functions could be changed to `(_ckd_value(FROM), _ckd_overflow(FROM))`
+ * and then `_ckd_value(FROM)` can be implicitly-promoted to `_ckd_value(TO)` type.
+ * But the problem is that then `FROM` is evaulated two times, and we do not want that.
+ */
+#define _ckd_arg(TO, FROM) \
+			_Generic(_ckd_toct(TO), \
+	{% call () L.foreach_TYPE(inmacro=1) %}
+			_ckd_c$TYPE: \
+				_Generic(_ckd_toct(FROM), \
+		{% call() L.foreach_TYPE(inmacro=1, repl="$TYPE2") %}
+				_ckd_c$TYPE2: _ckd_c$TYPE2_to_arg_$TYPE
+		{%- endcall %})
+	{%- endcall %})(_ckd_toct(FROM))
+
 // ]]]
 // Macros [[[
 {% call() L.foreach_OP() %}
 
-#define _ckd_$OP_3(r, a, b) \
-			_Generic(_ckd_value(a) + _ckd_value(b) + *(r), \
+#define _ckd_$OP_3_in(T, r, a, b) \
+			_Generic(T, \
 	{% call() L.foreach_TYPE(inmacro=1) %}
 			_ckd_$TYPE: _Generic(*(r), \
 		{% call() L.foreach_TYPE(inmacro=1, repl="$TYPE2") %}
 				_ckd_$TYPE2: _ckd_$OP_3_$TYPE_to_$TYPE2
 		{%- endcall %})
-	{%- endcall %})(r, _ckd_idx(a, b), _ckd_value(a), _ckd_value(b), _ckd_overflow(a) || _ckd_overflow(b))
+	{%- endcall %})(r, _ckd_arg(T, a), _ckd_arg(T, b))
 
-#define _ckd_$OP_2(a, b) \
-			_Generic(_ckd_value(a) + _ckd_value(b), \
+#define _ckd_$OP_3(r, a, b) \
+		_ckd_$OP_3_in(_ckd_value(a) + _ckd_value(b) + *(r), r, a, b)
+
+#define _ckd_$OP_2_in(T, a, b) \
+			_Generic(T, \
 	{% call() L.foreach_TYPE(inmacro=1) %}
 			_ckd_$TYPE: _ckd_$OP_2_$TYPE
-	{%- endcall %})(_ckd_idx(a, b), _ckd_value(a), _ckd_value(b), _ckd_overflow(a) || _ckd_overflow(b))
+	{%- endcall %})(_ckd_arg(T, a), _ckd_arg(T, b))
+
+#define _ckd_$OP_2(a, b) \
+		_ckd_$OP_2_in(_ckd_value(a) + _ckd_value(b), a, b)
+
 {% endcall %}
 
 // ]]]
