0f04a4ae2d4614490fbe8e8c69270846d82b5298
Kamil Cukrowski <kamilcukrowski@gmail.com>
2021-11-24 23:19:39 +0100

    Increase coverage to 100%

---
+++ tests/add_coverage.c
@@ -0,0 +1,10 @@
+#include "test.h"
+
+void test() {
+#if __GNUC__ && !__INTEL_COMPILER
+	CKDTEST(ckd_mul((__int128)-1, (__int128)1), -1, 0);
+	CKDTEST(ckd_mul((__int128)1, (__int128)-1), -1, 0);
+	CKDTEST(ckd_mul((__int128)0, (__int128)-1), 0, 0);
+#endif
+	CKDTEST(ckd_mul((long)-1, (long)1), -1, 0);
+}
