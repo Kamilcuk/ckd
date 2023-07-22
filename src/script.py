21a3f3eb88952cd7956cc72c67ea3aac31dbad64
Kamil Cukrowski <kamilcukrowski@gmail.com>
2023-07-22 16:02:19 +0200

    script.py: format black

--- src/script.py
+++ src/script.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
-import sys
 import os
-import jinja2
+import sys
 
+import jinja2
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(os.path.abspath(__file__))),
     undefined=jinja2.StrictUndefined,
     autoescape=False,
     trim_blocks=True,
-    lstrip_blocks=True
+    lstrip_blocks=True,
 )
 
 args = sys.argv
 inputfile = args.pop(1)
-outf = open(args.pop(1), 'w')
+outf = open(args.pop(1), "w")
 for aa in sys.argv[1:]:
-    tt = aa.split('=')
+    tt = aa.split("=")
     env.globals[tt[0]] = tt[1]
 
 print(
